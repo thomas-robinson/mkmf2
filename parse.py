a0ee741241ef00ce410898a9036df9d54e5bfa29
Thomas Robinson <thomas.robinson@noaa.gov>
2019-10-24 09:15:27 -0400

    Added some comments to the parser

--- parse.py
+++ parse.py
@@ -16,18 +16,22 @@ f.close()
 """ Set up the regex to find use statements ^\s*use """
 module_dependencies = []
 findUse = re.compile('^\s*use', re.IGNORECASE)
-justUse = re.compile('^\s*use', re.IGNORECASE)
+findOnly = re.compile('only\s*:', re.IGNORECASE)
+""" Parse each line of the file for the regex ^\s*use """
 if verbose or vv:
-	print('regex is ^\s*use')
+	print('Parse each line of the file for the regex ^\s*use ')
 for line in fcontents.splitlines():
 	if findUse.match(line):
 		if verbose or vv:
 			print(line)
+""" Split the line on a , because of multiple module possibility on a line """
 		splitModLineCom = line.split(",")
 		for mods in splitModLineCom:
+""" If the current string has USE in it, split on white space.  The second member of this list is a module """
 			if findUse.match(mods):
 				mod1 = mods.split()
 				alreadyExist = False
+""" Get rid of dupicate mentions of a module """
 				for md in module_dependencies:
 					if md == mod1[1]:
 						alreadyExist = True
