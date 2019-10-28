992f02812bc7abd4c924286acf0a826d218d42fd
Diyor Zakirov <Diyor.Zakirov@lscsky50-d.gfdl.noaa.gov>
2019-10-28 11:01:28 -0400

    pushing working parser

--- parse.py
+++ parse.py
@@ -1,5 +1,5 @@
 #!/bin/python
-import re
+import re;
 
 verbose = True
 vv = False
@@ -13,25 +13,57 @@ if vv:
 	print(fname, ":\n ",fcontens)
 f.close()
 
+
 """ Set up the regex to find use statements ^\s*use """
 module_dependencies = []
 findUse = re.compile('^\s*use', re.IGNORECASE)
 findOnly = re.compile('only\s*:', re.IGNORECASE)
-""" Parse each line of the file for the regex ^\s*use """
+findUseAmp = re.compile('^\s*use&', re.IGNORECASE)
+findUseAmpSpc = re.compile('^\s*use\s+&', re.IGNORECASE)
+
+
+'''Parse each line of the file for the regex ^\s*use '''
 if verbose or vv:
 	print('Parse each line of the file for the regex ^\s*use ')
-for line in fcontents.splitlines():
-	if findUse.match(line):
+for index, line in enumerate(fcontents.splitlines()):
+	if findUse.match(line):	 
 		if verbose or vv:
 			print(line)
-""" Split the line on a , because of multiple module possibility on a line """
-		splitModLineCom = line.split(",")
+		splitModLineCom = line.split(",")	
+		print(splitModLineCom)
 		for mods in splitModLineCom:
-""" If the current string has USE in it, split on white space.  The second member of this list is a module """
-			if findUse.match(mods):
+			if (findUseAmp.match(mods) or findUseAmpSpc.match(mods)):
+				if fcontents.splitlines()[index+1].split(",")[1]:
+					splitmodLineCom = fcontents.splitlines()[index+1]
+					print(splitmodLineCom)
+					for mods in splitModLineCom:
+						alreadyExists = False
+						for md in module_dependencies:
+							if md == mods[0]:
+								alreadyExists = True
+								if vv:
+									print(fname,": the module ",md," is listd more than once.  only keeping one")
+									break;
+							if not alreadyExists:
+								module_dependencies.append(mods[0])
+				else:
+					temp = 2
+					while not fcontents.splitlines()[index+temp][1]:
+						temp += 1
+					splitmodLineCom = fcontents.splitlines()[index+temp]
+					for mods in splitModLineCom:
+						alreadyExists = False
+						for md in module_dependencies:
+							if md == mods[0]:
+								alreadyExists = True
+								if vv:
+									print(fname,": the module ",md," is listd more than once.  only keeping one")
+									break;
+							if not alreadyExists:
+								module_dependencies.append(mods[0])				
+			if findUse.match(mods) and len(splitModLineCom) > 1:
 				mod1 = mods.split()
 				alreadyExist = False
-""" Get rid of dupicate mentions of a module """
 				for md in module_dependencies:
 					if md == mod1[1]:
 						alreadyExist = True
@@ -40,7 +72,11 @@ for line in fcontents.splitlines():
 						break
 				if not alreadyExist:
 					module_dependencies.append(mod1[1])
-
+		
+		
+		
+		
+		
 if verbose or vv:
 	print("The dependencies for ",fname," are ",module_dependencies)
 
---
+++ parseShort.py
@@ -0,0 +1,53 @@
+#!/bin/python
+import re;
+import os;
+
+fName = "diag_manager.F90"
+
+def getModules(fileName):
+	MODS = [] 
+	
+	fileContents = open(fileName).read()
+	
+	#The regex creates a pattern to find matches in the file
+	#Checks for USE and possible & on the same or next line, until module name is found signified by the '?'
+	#Ignores cases and tries to match on all lines
+	patternMatch = re.compile('^ *USE[ &\n]+.*?[ &\n]*,', re.IGNORECASE | re.M)  #regex to match 
+	
+	matches = re.findall(patternMatch, fileContents) #list of all matches
+	
+	for match in matches:
+		match = match.lower().strip()
+		badChars = ["use", "&", "\r", '\n', ' ', ',']
+		#clean up the matches
+		for char in badChars:
+			match = match.replace(char, '')
+		if not match in MODS:
+			MODS.append(match)
+			
+	
+	return MODS
+
+
+def writeModules(modules, path):
+	
+	makefile = open('Makefile.am', 'w')
+	
+	fileList = os.listdir(path)
+	
+	fortranMatch = re.compile('[.F90&]')
+	
+	makefile.write("noinst_LTLIBRARIES = " + path + ".la\n")
+	
+	for file in fileList:
+		if fotranMatch.match(file):
+			makefile.write(file + "\\n")
+		
+	for mod in modules:
+		makefile.write(mod + ".$(FC_MODEXT) : " + mod + ".$(OBJEXT)\n")
+	
+
+if __name__ == '__main__':
+	#testing diag_manager.F90
+	print(getModules(fName))
+	
